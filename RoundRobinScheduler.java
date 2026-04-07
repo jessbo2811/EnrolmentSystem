008b1b88036a84098f095e939c80a99f017741ac
Jess Bryant <jessmaybryantt@gmail.com>
2026-04-07 14:57:12 +0100

    created the EnrolmentProcess LinkedList

--- RoundRobinScheduler.java
+++ RoundRobinScheduler.java
@@ -1,1 +1,20 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class RoundRobinScheduler extends Thread {
+
+Queue<EnrolmentProcess> queue = new LinkedList<EnrolmentProcess>();
+
+    public startEnrolment() {
+
+        CompletedList<EnrolmentProcess> completed = new LinkedList<EnrolmentProcess>();
+
+        if (getState(EnrolmentProcess[0] === 'NEW')) {
+            
+        }
+
+
+
+
+    }
+}
