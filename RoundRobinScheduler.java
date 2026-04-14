36a7ec2a7cfee4f1675af100954ef536037a059b
Jess Bryant <jessmaybryantt@gmail.com>
2026-04-14 16:51:30 +0100

    implemented the startEnrolment(), dequeue() and enqueue() methods

--- RoundRobinScheduler.java
+++ RoundRobinScheduler.java
@@ -1,20 +1,59 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Queue;
 
-public class RoundRobinScheduler extends Thread {
+public class RoundRobinScheduler extends Thread { 
 
-Queue<EnrolmentProcess> queue = new LinkedList<EnrolmentProcess>();
+Queue<EnrolmentProcess> queue = new LinkedList<EnrolmentProcess>(); // creating the queue linked list of enrolment processes
 
-    public startEnrolment() {
+public enqueue(EnrolmentProcess x) {
 
-        CompletedList<EnrolmentProcess> completed = new LinkedList<EnrolmentProcess>();
+    queue.addFirst(x);
 
-        if (getState(EnrolmentProcess[0] === 'NEW')) {
+}
+
+public dequeue(EnrolmentProcess x, LinkedList y) {
+
+    y.addLast(x);
+    y.remove()
+
+}
+
+    public startEnrolment() { // function to start the enrolment processes
+
+        LinkedList<EnrolmentProcess> completed = new LinkedList<EnrolmentProcess>(); // creating the linked list of completed processes called 'completed'
+
+        Thread.state currentState = getState(queue[0]); // making the current state a variable to shorten the code I have to write within the if statements
+
+        if (currentState == 'NEW') {
+
+            queue[0].run();
+            
+            dequeue(queue[0], queue);
+
+        }
+
+        else if (currentState == 'TERMINATED') {
+
+            dequeue(queue[0], completed);
             
         }
 
+        else {
 
+            interrupt(queue[0]);
+
+            queue[0].run();
+
+            dequeue(queue[0], queue);
+
+        }
+
+        if (queue.isEmpty()) {
+
+           // completed.printList(); implement later
 
 
     }
+}
 }
